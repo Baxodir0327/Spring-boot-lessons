d7dc35760e2f525c37baa41f351f3752fc2f9451
bpard <bpardaboyev2000@gmail.com>
2023-10-17 19:27:37 +0500

    first commit

---
+++ src/main/java/com/company/appspringbootfirst/AppSpringBootFirstApplication.java
@@ -0,0 +1,13 @@
+package com.company.appspringbootfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpringBootFirstApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppSpringBootFirstApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/appspringbootfirst/AppSpringBootFirstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.appspringbootfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpringBootFirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
